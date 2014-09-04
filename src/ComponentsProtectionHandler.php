e2678a429cb99a0e14fd3a8630928f48fc8e09d7
Jáchym Toušek <enumag@gmail.com>
2014-09-04 12:47:39 +0200

    Renamed variable

--- src/ComponentsProtectionHandler.php
+++ src/ComponentsProtectionHandler.php
@@ -25,32 +25,32 @@ class ComponentsProtectionHandler extends Object implements IRuleHandler
 {
 
 	/**
-	 * @param Actions $annotation
+	 * @param Actions $rule
 	 * @param Request $request
 	 * @param string $component
 	 * @throws ComponentInaccessibleException
 	 */
-	public function checkRule(IRule $annotation, Request $request, $component = NULL)
+	public function checkRule(IRule $rule, Request $request, $component = NULL)
 	{
-		if ($annotation instanceof Actions) {
-			$this->checkRuleActions($annotation, $request);
+		if ($rule instanceof Actions) {
+			$this->checkRuleActions($rule, $request);
 		} else {
-			throw new InvalidArgumentException('Unknown annotation \'' . get_class($annotation) . '\' given.');
+			throw new InvalidArgumentException('Unknown rule \'' . get_class($rule) . '\' given.');
 		}
 	}
 
 	/**
-	 * @param Actions $annotation
+	 * @param Actions $rule
 	 * @param Request $request
 	 * @throws ComponentInaccessibleException
 	 */
-	protected function checkRuleActions(Actions $annotation, Request $request)
+	protected function checkRuleActions(Actions $rule, Request $request)
 	{
 		$parameters = $request->getParameters();
-		if ($annotation->actions === array('*')) {
+		if ($rule->actions === array('*')) {
 			return;
 		}
-		if (!in_array($parameters[Presenter::ACTION_KEY], $annotation->actions)) {
+		if (!in_array($parameters[Presenter::ACTION_KEY], $rule->actions)) {
 			throw new ComponentInaccessibleException("Component is inaccessible for the given action.");
 		}
 	}
